95b6a6750b02ccf30b74cd9a164fa5ef6b1eeb5a
Aaron Delate <aaron@zero-one.io>
2025-07-19 10:02:19 +0200

    crimesjohnson: solve maze problem

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -9,16 +9,45 @@
 /// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
 ///
 /// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
+/// If a direction is true, then we can proceed.
 ///
 /// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!".  If there is no wall,
 /// then the 'currX' and 'currY' values should be changed.
 /// </summary>
 public class Maze
 {
+    private const int LeftMoveAllowedIdx = 0;
+    private const int RightMoveAllowedIdx = 1;
+    private const int UpMoveAllowedIdx = 2;
+    private const int DownMoveAllowedIdx = 3;
     private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
     private int _currX = 1;
     private int _currY = 1;
+
+    private ValueTuple<int, int> currPosKey()
+    {
+        return (_currX, _currY);
+    }
+
+    private ValueTuple<int, int> rightMoveKey()
+    {
+        return (_currX + 1, _currY);
+    }
+
+    private ValueTuple<int, int> leftMoveKey()
+    {
+        return (_currX - 1, _currY);
+    }
+
+    private ValueTuple<int, int> downMoveKey()
+    {
+        return (_currX, _currY + 1);
+    }
+
+    private ValueTuple<int, int> upMoveKey()
+    {
+        return (_currX, _currY - 1);
+    }
 
     public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
     {
@@ -32,7 +61,10 @@ public class Maze
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        if (_mazeMap.Keys.Contains(leftMoveKey()) && _mazeMap[currPosKey()][LeftMoveAllowedIdx])
+            _currX--;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
     /// <summary>
@@ -41,7 +73,10 @@ public class Maze
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        if (_mazeMap.Keys.Contains(rightMoveKey()) && _mazeMap[currPosKey()][RightMoveAllowedIdx])
+            _currX++;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
     /// <summary>
@@ -50,7 +85,10 @@ public class Maze
     /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        if (_mazeMap.Keys.Contains(upMoveKey()) && _mazeMap[currPosKey()][UpMoveAllowedIdx])
+            _currY--;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
     /// <summary>
@@ -59,7 +97,10 @@ public class Maze
     /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        if (_mazeMap.Keys.Contains(downMoveKey()) && _mazeMap[currPosKey()][DownMoveAllowedIdx])
+            _currY++;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
     public string GetStatus()
